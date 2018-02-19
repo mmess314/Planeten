9b3748c7762759ba5b3195990ab3b3b51c8867d0
mmess314 <34944042+mmess314@users.noreply.github.com>
2018-02-19 14:54:58 +0000

    Update Program.cs
    G limited to 4 significant digits

--- Planeten9/Planeten9/Program.cs
+++ Planeten9/Planeten9/Program.cs
@@ -6,7 +6,7 @@ public class Program
     const double G = 6.674E-11; // gravitational constant [m^3/(kg s^2)] 
     const double MSonne = 1.98892E30; // mass of sun [kg]
     const double K = G * MSonne;
-    const double AE = 1.49597870700E11; // Astronomische Einheit [m]
+    const double AE = 1.49597870700E11; // unit distance [m]
     const double J = 31536000; // 31536000 seconds in a year
 
     const int TEnde = 31536000; // 1 Jahr
