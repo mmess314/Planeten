c94ea78907dfa1941a33ad1225c4bab114681275
mmess314 <34944042+mmess314@users.noreply.github.com>
2018-02-19 14:34:52 +0000

    Update Program.cs

--- Planeten9/Planeten9/Program.cs
+++ Planeten9/Planeten9/Program.cs
@@ -3,8 +3,8 @@
 public class Program
 {
     //Konstanten
-    const double G = 6.67408E-11; //Gravitationskonstante [m^3/(kg s^2)] 
-    const double MSonne = 1.98892E30; // Sonnenmasse [kg]
+    const double G = 6.674E-11; // gravitational constant [m^3/(kg s^2)] 
+    const double MSonne = 1.98892E30; // mass of sun [kg]
     const double K = G * MSonne;
     const double AE = 1.49597870700E11; // Astronomische Einheit [m]
     const double J = 31536000; // 31536000 seconds in a year
